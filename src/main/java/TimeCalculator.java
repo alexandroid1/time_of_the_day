64a7ed95352bf8b0e28b90074bdd86bbd78eba3a
Oleksandr <avpavlov96@gmail.com>
2016-04-02 20:10:16 +0300

    getTimeOfTheDay method

--- src/main/java/TimeCalculator.java
+++ src/main/java/TimeCalculator.java
@@ -5,15 +5,35 @@ import java.util.Calendar;
  */
 public class TimeCalculator {
 
-    private int minutes;
     private int hours;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
 
     public void getTime()
     {
         Calendar calendar = Calendar.getInstance();
         calendar.getTime();
-        this.minutes = calendar.MINUTE;
-        this.hours= calendar.HOUR_OF_DAY;
+        this.hours= calendar.get(Calendar.HOUR_OF_DAY);
+    }
+
+    public void getTimeOfTheDay(int hours)
+    {
+        if (hours > 6 && hours <= 9 ) {
+            System.out.println("Good morning, World!");
+        } else if (hours > 9 && hours <= 19 ) {
+            System.out.println("Good day, World!");
+        } else if (hours > 19 && hours <= 23 ) {
+            System.out.println("Good evening, World!");
+        } else if (hours > 23 && hours <= 24 || hours >= 0 && hours <= 6 ) {
+            System.out.println("Good night, World!");
+        }
+
     }
 
 
