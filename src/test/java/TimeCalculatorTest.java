bae5a9e3e2bd47aeb49f18ca7a2c1957ba1ed732
Oleksandr <avpavlov96@gmail.com>
2016-04-03 13:11:05 +0300

    locale improved

--- src/test/java/TimeCalculatorTest.java
+++ src/test/java/TimeCalculatorTest.java
@@ -1,6 +1,11 @@
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Locale;
+import java.util.Properties;
 
 /**
  * Created by ALEX on 02.04.2016.
@@ -11,10 +16,36 @@ public class TimeCalculatorTest {
 
         if (Locale.getDefault().toString().equals("ru_RU")){
 
-            String morning = "Доброе утро, Мир!";
-            String day = "Добрый день, Мир!";
-            String evening = "Добрый вечер, Мир!";
-            String night = "Спокойной ночи, Мир!";
+            FileInputStream fis;
+            Properties property = new Properties();
+
+            try {
+                String filePath = "./src/main/resources/"+Locale.getDefault().toString()+".properties";
+                fis = new FileInputStream(filePath);
+                property.load(fis);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String morning = property.getProperty("morning");
+            String day = property.getProperty("day");
+            String evening = property.getProperty("evening");
+            String night = property.getProperty("night");
+
+            try {
+                byte[] morningData = morning.getBytes("ISO-8859-1");
+                byte[] dayData = day.getBytes("ISO-8859-1");
+                byte[] eveningData = evening.getBytes("ISO-8859-1");
+                byte[] nightData = night.getBytes("ISO-8859-1");
+                morning = new String(morningData);
+                day = new String(dayData);
+                evening = new String(eveningData);
+                night = new String(nightData);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            };
 
             final TimeCalculator calculator = new TimeCalculator();
             String testDay = calculator.getTimeOfTheDay();
